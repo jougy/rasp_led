50182d086ed9260599479e846e81446d14eaac64
jougy <jougygames@gmail.com>
2019-07-09 20:45:19 +0000

    primeira integracao com sensor de umidade e temperatura

--- rasp_led.py
+++ rasp_led.py
@@ -1,19 +1,50 @@
 import RPi.GPIO as GPIO
 import time
 import sys
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(18,GPIO.OUT)
+import os
+import Adafruit_DHT
+SLEEP_TIME = 0.5
+LED_PIN = 18
+DHT_PIN = 4
 
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LED_PIN,GPIO.OUT)
+
+SENSOR = Adafruit_DHT.DHT11
+
+def high():
+    GPIO.output(LED_PIN, GPIO.HIGH)
+
+def low():
+    GPIO.output(LED_PIN, GPIO.LOW)
+
+def get_hum_temp():
+    humidity, temperature = Adafruit_DHT.read_retry(SENSOR, DHT_PIN)
+    return humidity, temperature
+def oF():
+    temperature = get_hum_temp()[1]
+    return((temperature*(9/5))+32)
+    
 print("sys.argv vale {}".format(sys.argv))
 if len(sys.argv) >= 2:
     if sys.argv[1] == 'high':
-        GPIO.output(18, GPIO.HIGH)
+        high()
     elif sys.argv[1] == 'low':
-        GPIO.output(18, GPIO.LOW)
+        low()
 
 else:
-    while(True):
-        GPIO.output(18, GPIO.HIGH)
-        time.sleep(1)
-        GPIO.output(18, GPIO.LOW)
-        time.sleep(1)
+    try:
+        while(True):
+            high()
+            humidity, temperature = get_hum_temp()
+            temp_f = oF()
+            print("umidade: {}\ntemperatura: {}C / {}F ".format(humidity,temperature,temp_f))
+            time.sleep(SLEEP_TIME)
+            low()
+            time.sleep(SLEEP_TIME)
+    except KeyboardInterrupt:
+        low()
+        os.system('clear')
+        print("saindo do programa...")
+        sys.exit()
