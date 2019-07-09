3a7ef2d9a4ee351c05f13f0d79e3e6047c6048f2
victortv7@gmail.com <victortv7@gmail.com>
2019-07-09 18:26:52 +0000

    meu primeiro commit

---
+++ rasp_led.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+import time
+import sys
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18,GPIO.OUT)
+
+#if sys.argv[1] == 'high':
+#    GPIO.output(18, GPIO.HIGH)
+#elif sys.argv[1] == 'low':
+#    GPIO.output(18, GPIO.LOW)
+
+else:
+    while(True):
+        GPIO.output(18, GPIO.HIGH)
+        time.sleep(1)
+        GPIO.output(18, GPIO.LOW)
+        time.sleep(1)
